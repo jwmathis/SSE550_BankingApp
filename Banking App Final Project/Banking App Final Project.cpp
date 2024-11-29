341489b77c274a31bfaa2800552822cbb33ead78
jwmathis <jwmathis7@gmail.com>
2024-11-28 20:40:39 -0500

    added spinning dollar sign logo

--- Banking App Final Project/Banking App Final Project.cpp
+++ Banking App Final Project/Banking App Final Project.cpp
@@ -10,6 +10,9 @@
 #include "Transfer.h"
 #include "sqlite3.h" // For database management
 #include <iomanip>
+#include <thread>
+#include <chrono>
+#include <conio.h>
 
 using namespace std;
 bool printedHeaders = false;
@@ -54,6 +57,132 @@ static int callback(void* NotUsed, int argc, char** argv, char** azColName);
 //static int selectUserData(const char* s, const string& username);
 //static int selectAccounts(const char* s, const string& username);
 
+void clearScreen() {
+	// This works for Windows; for UNIX, use "clear"
+	system("cls");
+}
+
+void moveToPosition(int row, int col) {
+	cout << "\033[" << row << ";" << col << "H";
+}
+
+void printDollarSign(int frame, int row, int col) {
+	moveToPosition(row, col);
+	switch (frame) {
+	case 0: // Initial frame
+		cout << "      $$$$       " << endl;
+		moveToPosition(row + 1, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 2, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 3, col);
+		cout << "   $$            " << endl;
+		moveToPosition(row + 4, col);
+		cout << "    $$$$$$       " << endl;
+		moveToPosition(row + 5, col);
+		cout << "         $$      " << endl;
+		moveToPosition(row + 6, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 7, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 8, col);
+		cout << "      $$$$       " << endl;
+		break;
+	case 1: // Slight rotation
+		cout << "       $$        " << endl;
+		moveToPosition(row + 1, col);
+		cout << "      $$$$       " << endl;
+		moveToPosition(row + 2, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 3, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 4, col);
+		cout << "   $$            " << endl;
+		moveToPosition(row + 5, col);
+		cout << "    $$$$$$       " << endl;
+		moveToPosition(row + 6, col);
+		cout << "         $$      " << endl;
+		moveToPosition(row + 7, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 8, col);
+		cout << "    $$$  $$$     " << endl;
+		break;
+	case 2: // More rotation
+		cout << "       $$        " << endl;
+		moveToPosition(row + 1, col);
+		cout << "       $$        " << endl;
+		moveToPosition(row + 2, col);
+		cout << "      $$$$       " << endl;
+		moveToPosition(row + 3, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 4, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 5, col);
+		cout << "   $$            " << endl;
+		moveToPosition(row + 6, col);
+		cout << "    $$$$$$       " << endl;
+		moveToPosition(row + 7, col);
+		cout << "         $$      " << endl;
+		moveToPosition(row + 8, col);
+		cout << "   $$      $$    " << endl;
+		break;
+	case 3: // Further rotation
+		cout << "         $$      " << endl;
+		moveToPosition(row + 1, col);
+		cout << "       $$        " << endl;
+		moveToPosition(row + 2, col);
+		cout << "       $$        " << endl;
+		moveToPosition(row + 3, col);
+		cout << "      $$$$       " << endl;
+		moveToPosition(row + 4, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 5, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 6, col);
+		cout << "   $$            " << endl;
+		moveToPosition(row + 7, col);
+		cout << "    $$$$$$       " << endl;
+		moveToPosition(row + 8, col);
+		cout << "         $$      " << endl;
+		break;
+	case 4: // Almost back to original
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 1, col);
+		cout << "         $$      " << endl;
+		moveToPosition(row + 2, col);
+		cout << "       $$        " << endl;
+		moveToPosition(row + 3, col);
+		cout << "       $$        " << endl;
+		moveToPosition(row + 4, col);
+		cout << "      $$$$       " << endl;
+		moveToPosition(row + 5, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 6, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 7, col);
+		cout << "   $$            " << endl;
+		break;
+	case 5: // Back to original position but flipped
+		cout << "      $$$$       " << endl;
+		moveToPosition(row + 1, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 2, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 3, col);
+		cout << "         $$      " << endl;
+		moveToPosition(row + 4, col);
+		cout << "    $$$$$$       " << endl;
+		moveToPosition(row + 5, col);
+		cout << "   $$            " << endl;
+		moveToPosition(row + 6, col);
+		cout << "   $$      $$    " << endl;
+		moveToPosition(row + 7, col);
+		cout << "    $$$  $$$     " << endl;
+		moveToPosition(row + 8, col);
+		cout << "      $$$$       " << endl;
+		break;
+	}
+}
 
 int main() {
 	// Open/Create the database
@@ -61,8 +190,25 @@ int main() {
 	createDB(databaseDir);
 	createTable(databaseDir);
 	Bank MercerBank(databaseDir); //initialize bank and database
+	bool exitFlag = false;
 
-	cout << WELCOME_MESSAGE << endl; // Display welcome message
+	const int frames = 6; // Total number of frames for the spin
+	cout << WELCOME_MESSAGE << "\n" << endl; // Display welcome message
+	while (!exitFlag) {
+		for (int i = 0; i < frames; ++i) {
+			//clearScreen(); // Clear the console
+			printDollarSign(i, 5, 20); // Print the current frame
+			std::this_thread::sleep_for(std::chrono::milliseconds(150)); // Pause for a moment
+
+			if (_kbhit()) {
+				char ch = _getch();
+				if (ch == 13) { //ASCII for enter key
+					exitFlag = true;
+					break;
+				}
+			}
+		}
+	}
 
 	// Display Main menu options
 	while (true) {
