c84c14d6dae9a746f9a14f8b2f9a13abbd36a70c
jwmathis7 <jwmathis7@gmail.com>
2024-12-13 13:36:05 -0500

    changed dollar sign to flashing

--- Banking App Final Project/UIHelpers.h
+++ Banking App Final Project/UIHelpers.h
@@ -1,9 +1,9 @@
 #pragma once
 // Functions for animated dollar sign logo
-void displayWelcomeAnimation();
-void clearScreen();
+void displayWelcomeAnimation(void);
+void clearScreen(void);
 void moveToPosition(int row, int col);
-void printDollarSign(int frame, int row, int col);
+void printDollarSign(void);
 
 // Function for MU logo
 void printMU();
